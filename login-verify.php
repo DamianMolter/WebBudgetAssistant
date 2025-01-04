2292e3fb4f51e594265b84ce3c4e2133ed898482
Damian Molter <damian.molter.programista@gmail.com>
2025-01-04 03:03:57 +0100

    Added email verification

--- login-verify.php
+++ login-verify.php
@@ -5,18 +5,15 @@ if (isset($isUserLogged)) {
 }
 
 $config = require_once 'connect.php';
-$email = $_POST["email"];
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 $password = $_POST["password"];
+
 
 try {
       $db = new PDO("mysql:host={$config['host']}; dbname={$config['database']}; charset=utf8", $config['user'], $config['password'],[
             PDO::ATTR_EMULATE_PREPARES => false,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      ]
-      
-      );
+      ]);
 } catch (PDOException $error) {
       exit('Database error');
 }
-
-echo "Połączono z bazą danych";
