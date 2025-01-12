72b7336db8a9158365852ec1edd39f8d464833a9
Damian Molter <damian.molter.programista@gmail.com>
2025-01-12 01:03:12 +0100

    Added database to repository

---
+++ budgetassistant.sql
@@ -0,0 +1,342 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sty 12, 2025 at 10:38 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `budgetassistant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `expense_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `payment_method_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT 0.00,
+  `date_of_expense` date NOT NULL,
+  `expense_comment` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses_category_assigned_to_users`
+--
+
+CREATE TABLE `expenses_category_assigned_to_users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expenses_category_default`
+--
+
+CREATE TABLE `expenses_category_default` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `expenses_category_default`
+--
+
+INSERT INTO `expenses_category_default` (`id`, `name`) VALUES
+(1, 'Food'),
+(2, 'Fuel'),
+(3, 'City transport'),
+(4, 'Taxi'),
+(5, 'Fun'),
+(6, 'Health'),
+(7, 'Clothes'),
+(8, 'Hygiene'),
+(9, 'Kids'),
+(10, 'Recreation'),
+(11, 'Travel'),
+(12, 'Savings'),
+(13, 'For pension'),
+(14, 'Debt repayment'),
+(15, 'Presents'),
+(16, 'Another');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes`
+--
+
+CREATE TABLE `incomes` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `income_category_assigned_to_user_id` int(11) UNSIGNED NOT NULL,
+  `amount` decimal(8,2) NOT NULL DEFAULT 0.00,
+  `date_of_income` date NOT NULL,
+  `income_comment` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes_category_assigned_to_users`
+--
+
+CREATE TABLE `incomes_category_assigned_to_users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `incomes_category_default`
+--
+
+CREATE TABLE `incomes_category_default` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `incomes_category_default`
+--
+
+INSERT INTO `incomes_category_default` (`id`, `name`) VALUES
+(1, 'Paycheck'),
+(2, 'Investments'),
+(3, 'Passive income'),
+(4, 'Another');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `payment_methods_assigned_to_users`
+--
+
+CREATE TABLE `payment_methods_assigned_to_users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `payment_methods_default`
+--
+
+CREATE TABLE `payment_methods_default` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `payment_methods_default`
+--
+
+INSERT INTO `payment_methods_default` (`id`, `name`) VALUES
+(1, 'Debit card'),
+(2, 'Cash'),
+(3, 'Credit card');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `expense_category_assigned_to_user_id` (`expense_category_assigned_to_user_id`),
+  ADD KEY `payment_method_assigned_to_user_id` (`payment_method_assigned_to_user_id`);
+
+--
+-- Indeksy dla tabeli `expenses_category_assigned_to_users`
+--
+ALTER TABLE `expenses_category_assigned_to_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeksy dla tabeli `expenses_category_default`
+--
+ALTER TABLE `expenses_category_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `incomes`
+--
+ALTER TABLE `incomes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `income_category_assigned_to_user_id` (`income_category_assigned_to_user_id`);
+
+--
+-- Indeksy dla tabeli `incomes_category_assigned_to_users`
+--
+ALTER TABLE `incomes_category_assigned_to_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeksy dla tabeli `incomes_category_default`
+--
+ALTER TABLE `incomes_category_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `payment_methods_assigned_to_users`
+--
+ALTER TABLE `payment_methods_assigned_to_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indeksy dla tabeli `payment_methods_default`
+--
+ALTER TABLE `payment_methods_default`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `expenses_category_assigned_to_users`
+--
+ALTER TABLE `expenses_category_assigned_to_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
+-- AUTO_INCREMENT for table `expenses_category_default`
+--
+ALTER TABLE `expenses_category_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `incomes`
+--
+ALTER TABLE `incomes`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `incomes_category_assigned_to_users`
+--
+ALTER TABLE `incomes_category_assigned_to_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `incomes_category_default`
+--
+ALTER TABLE `incomes_category_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `payment_methods_assigned_to_users`
+--
+ALTER TABLE `payment_methods_assigned_to_users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `payment_methods_default`
+--
+ALTER TABLE `payment_methods_default`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD CONSTRAINT `expenses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `expenses_ibfk_2` FOREIGN KEY (`expense_category_assigned_to_user_id`) REFERENCES `expenses_category_assigned_to_users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `expenses_ibfk_3` FOREIGN KEY (`payment_method_assigned_to_user_id`) REFERENCES `payment_methods_assigned_to_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `expenses_category_assigned_to_users`
+--
+ALTER TABLE `expenses_category_assigned_to_users`
+  ADD CONSTRAINT `expenses_category_assigned_to_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `incomes`
+--
+ALTER TABLE `incomes`
+  ADD CONSTRAINT `incomes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `incomes_ibfk_2` FOREIGN KEY (`income_category_assigned_to_user_id`) REFERENCES `incomes_category_assigned_to_users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `incomes_category_assigned_to_users`
+--
+ALTER TABLE `incomes_category_assigned_to_users`
+  ADD CONSTRAINT `incomes_category_assigned_to_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `payment_methods_assigned_to_users`
+--
+ALTER TABLE `payment_methods_assigned_to_users`
+  ADD CONSTRAINT `payment_methods_assigned_to_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
